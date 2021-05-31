679657dc113970f023aa340d4f29e9eb4d3d5960
Anmol Joshi <anmol01joshi@gmail.com>
2021-06-01 00:36:51 +0530

    update

--- ip-finder.js
+++ ip-finder.js
@@ -1,6 +1,6 @@
 const ipOutput = document.querySelector('#ip-finder-output');
 ipOutput.innerHTML = '...';
-fetch('http://api.ipify.org/?format=json')
+fetch('https://api.ipify.org/?format=json')
     .then((response) => response.json())
     .then((data) => {
         ipOutput.innerHTML = data.ip;
